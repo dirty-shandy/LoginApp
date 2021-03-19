592790a88491eee5e44c3ce4b8d8472da91844b7
Alexander Kagan <51817049+dirty-shandy@users.noreply.github.com>
2021-03-19 18:21:00 +0200

    LoginApp
    C# windows form application, 
    connect to SQL database 
    UserName - user1
    Password - 12345

---
+++ loginSystem/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace loginSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.usernametb = new System.Windows.Forms.TextBox();
+            this.passwordtb = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(386, 92);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(95, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login App";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // usernametb
+            // 
+            this.usernametb.Location = new System.Drawing.Point(72, 155);
+            this.usernametb.Name = "usernametb";
+            this.usernametb.PlaceholderText = "UserName";
+            this.usernametb.Size = new System.Drawing.Size(236, 31);
+            this.usernametb.TabIndex = 1;
+            this.usernametb.TextChanged += new System.EventHandler(this.UserName_TextChanged);
+            // 
+            // passwordtb
+            // 
+            this.passwordtb.Location = new System.Drawing.Point(72, 230);
+            this.passwordtb.Name = "passwordtb";
+            this.passwordtb.PlaceholderText = "Password";
+            this.passwordtb.Size = new System.Drawing.Size(236, 31);
+            this.passwordtb.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(95, 423);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 52);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 525);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.passwordtb);
+            this.Controls.Add(this.usernametb);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox usernametb;
+        private System.Windows.Forms.TextBox passwordtb;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ loginSystem/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loginSystem
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\sasha\OneDrive\מסמכים\LogimForm.mdf;Integrated Security=True;Connect Timeout=30");   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("select COUNT(*) FROM UserTable where UserName = '" + usernametb.Text + "'and Password= '"+ passwordtb.Text +"'" , cn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                new HomeForm().Show();
+            }
+            else
+                MessageBox.Show("Invalid Username or Password");
+
+
+            cn.Close();
+
+
+        }
+
+        private void UserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
